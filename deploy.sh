c92559b9481d658217e982092b01bf1e01f4ee62
안광현 <agh1594@naver.com>
2026-04-27 10:11:09 +0900

    Change deploy to docker image based

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
-cd /home/ec2-user/AWS-Spring-Project
 
-echo "🔄 최신 코드 가져오기"
-git pull origin main
-
-echo "🐳 도커 이미지 빌드"
-docker build -t first-project .
+echo "🐳 최신 Docker 이미지 가져오기"
+docker pull pangja/aws-spring-project:latest
 
 echo "🛑 기존 컨테이너 중지 및 삭제"
 docker stop first-project-container || true
 docker rm first-project-container || true
 
 echo "🚀 새 컨테이너 실행"
-docker run -d --name first-project-container -p 8081:8080 first-project
+docker run -d \
+  --name first-project-container \
+  -p 8081:8080 \
+  pangja/aws-spring-project:latest
 
-echo "✅ 도커 배포 완료"
+echo "✅ 이미지 기반 배포 완료"
